fbe49d2e4e25e5ec6d77588cc47bd2c826d2b709
Hígor Pinheiro Costa <63820192+higorcos@users.noreply.github.com>
2020-12-22 00:32:18 -0300

    Config post

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
-
-
+const bodyParser = require('body-parser'); //
+//EJS
 app.set('view engine', 'ejs'); //motor html //estou dizendo para o express usar o EJS como View engine 
 app.use(express.static('public'));
-
+//Body Parser
+app.use(bodyParser.urlencoded({extended: false})); // usado para traduzir os dados passados no formulario 
+app.use(bodyParser.json()); // os dados são enviados via json 
+//Rotas
 app.get('/', (req, res) => {
 
   res.render('index');//vai mostra o html pelo EJS que tá na pasta "views"// só coloca  o nome do arquivo não precisa colocar o diretório completo.//passa a variavel para o html 
@@ -12,5 +15,10 @@ app.get('/', (req, res) => {
 app.get('/perguntar',(req,res)=>{
   res.render('perguntar');//nome do arguivo ejs
 })
+app.post('/salvarPergunta',(req,res)=>{
+  var titulo = req.body.titulo;
+  var descrição = req.body.descrição
+  res.send(`Formulário recebido com sucesso !//////////////// ${titulo}${descrição}`);
+});
 
 app.listen(8181, () => { console.log('App rodando'); });
